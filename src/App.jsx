141bfe8eb0ac21618430a489e0db2cabf780dc4a
anurag3345 <as3342507@gmail.com>
2025-09-19 14:02:09 +0545

    Nav and footer component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Navbar from "./components/nav";
+import Footer from "./components/footer";
 
 function App() {
 
   return (
     <>
-      <div>This is div</div>
-      <p> This is p</p>
+    <Navbar />
+    <Footer />
     </>
   );
 }
