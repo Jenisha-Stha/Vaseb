66b55cc17bca5bc3a782d85d99c52e24464373b7
Jenisha Shrestha <1jenishastha@gmail.com>
2025-09-19 17:55:41 +0545

    created components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-
 import './App.css'
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <>
-      <div>this is our ecommerce web</div>
-      <p>woah woah</p>
+     <Navbar />
+     <Hero />
     </>
   );
-}
-
+} 
 export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>hey im the hero of my life.</div>
+  )
+}
+
+export default Hero
